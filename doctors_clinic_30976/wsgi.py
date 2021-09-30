5951b8fe0cb1811e87987942aecf92766c5b70da
Crowdbotics <team@crowdbotics.com>
2021-09-30 02:24:34 +0000

    Update models

--- doctors_clinic_30976/wsgi.py
+++ doctors_clinic_30976/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doctors_clinic_30976.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "doctors_clinic_30976.settings")
 
 application = get_wsgi_application()
